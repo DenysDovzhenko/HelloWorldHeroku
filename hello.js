de869c0e58578918b19902b375741b96ba24bc76
Denys Dovzhenko <denis.dovzhenko1@gmail.com>
2024-03-20 09:42:50 +0200

    Added new content instead of Hello World!

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-   res.send('Hello World!'); 
+   res.send('We want to show new content here.'); 
 });
 
 app.listen(process.env.PORT, function () {

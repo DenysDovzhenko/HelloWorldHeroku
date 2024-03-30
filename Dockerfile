0d70d1f49aa0402e33cae371424de03fe35a9150
Denys Dovzhenko <denis.dovzhenko1@gmail.com>
2024-03-30 14:37:34 +0200

    Added SSL certificate implementation

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /usr/app
 RUN npm install express
 COPY hello.js .
 ENV PORT=80
-EXPOSE 80
+EXPOSE 80 443
 CMD node hello.js
--- hello.js
+++ hello.js
@@ -1,10 +1,28 @@
 var express = require('express');
+var http = require('http');
+var fs = require('fs');
 var app = express();
 
+const key = '/etc/letsencrypt/live/denjia74.duckdns.org/privkey.pem'
+const cert = '/etc/letsencrypt/live/denjia74.duckdns.org/fullchain.pem'
+
+if (fs.existsSync(key) && fs.existsSync(cert)) {
+    var https = require('https');
+
+    var options = {
+        key: fs.readFileSync('/etc/letsencrypt/live/denjia74.duckdns.org/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/denjia74.duckdns.org/fullchain.pem'),
+    }
+
+    https.createServer(options, app).listen(process.env.PORT, function () {
+        console.log('App is listening on port ' + process.env.PORT);
+    });
+}
+
 app.get('/', function (req, res) {
-   res.send('Hello World! Hello to everybody! We are going to post here new content soon!'); 
+    res.send('Hello World! Hello to everybody! We are going to post here new content soon!'); 
 });
 
-app.listen(process.env.PORT, function () {
-   console.log('App is listening on port ' + process.env.PORT);
+http.createServer(app).listen(process.env.PORT, function () {
+    console.log('App is listening on port ' + process.env.PORT);
 });

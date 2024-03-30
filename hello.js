09083dfe5bd822a7d1dfd5667e2f335caf409eb2
Denys Dovzhenko <denis.dovzhenko1@gmail.com>
2024-03-30 16:24:50 +0200

    Define app.get before https

--- hello.js
+++ hello.js
@@ -6,6 +6,10 @@ var app = express();
 const key = '/ssl/privkey.pem'
 const cert = '/ssl/fullchain.pem'
 
+app.get('/', function (req, res) {
+    res.send('Hello World! Hello to everybody! We are going to post here new content soon!'); 
+});
+
 if (fs.existsSync(key) && fs.existsSync(cert)) {
     var https = require('https');
 
@@ -18,10 +22,6 @@ if (fs.existsSync(key) && fs.existsSync(cert)) {
         console.log('App is listening on port ' + process.env.PORT);
     });
 }
-
-app.get('/', function (req, res) {
-    res.send('Hello World! Hello to everybody! We are going to post here new content soon!'); 
-});
 
 http.createServer(app).listen(process.env.PORT, function () {
     console.log('App is listening on port ' + process.env.PORT);
